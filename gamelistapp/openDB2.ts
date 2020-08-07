6a477b4976090b7a64adab2e852d0aeaa6466dcf
admin <admin@gmail.com>
2020-08-07 14:07:27 +0900

    add

--- gamelistapp/openDB2.ts
+++ gamelistapp/openDB2.ts
@@ -3,7 +3,7 @@ import sqlite3 from 'sqlite3';
 
 export async function openDB2(){
     return open({
-        filename: '/gamelist.sqlite',
+        filename: 'gamelist.sqlite',
         driver: sqlite3.Database
       })
 }
